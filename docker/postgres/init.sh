efffa89f2cfcbfb706d4a25af7d87c63c5b12431
Egidio Caprino <egidio.caprino@gmail.com>
2023-12-03 17:05:07 +0100

    feat(docker): add Nextcloud AIO

--- docker/postgres/init.sh
+++ docker/postgres/init.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER nextcloud PASSWORD '$(cat /run/secrets/postgres_nextcloud_password)';
-	CREATE DATABASE nextcloud;
-	GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+# 	CREATE USER nextcloud PASSWORD '$(cat /run/secrets/postgres_nextcloud_password)';
+# 	CREATE DATABASE nextcloud;
+# 	GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
+# EOSQL
