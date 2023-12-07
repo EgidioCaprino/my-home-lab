b3e92d7ab4483dd600f75c55c18fc3e9a9f399ce
Egidio Caprino <egidio.caprino@gmail.com>
2023-12-07 12:13:22 +0100

    chore(docker): delete postgres folder

--- docker/postgres/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-# 	CREATE USER nextcloud PASSWORD '$(cat /run/secrets/postgres_nextcloud_password)';
-# 	CREATE DATABASE nextcloud;
-# 	GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
-# EOSQL
